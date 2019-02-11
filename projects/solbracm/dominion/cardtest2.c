c1aee14ea305c7ce5950926c53c877bc63aeed33
Matthew Solbrack <solbracm@flip3.engr.oregonstate.edu>
2019-02-10 18:27:57 -0800

    Final Assignment 3 changes 	new file:   .Makefile.swp 	modified:   Makefile 	modified:   cardtest1 	new file:   cardtest1.gcda 	new file:   cardtest1.gcno 	modified:   cardtest2 	modified:   cardtest2.c 	modified:   cardtest2.gcda 	modified:   cardtest2.gcno 	new file:   cardtest3 	new file:   cardtest3.c 	renamed:    cardtest2.gcda -> cardtest3.gcda 	new file:   cardtest3.gcno 	new file:   cardtest4 	new file:   cardtest4.c 	new file:   cardtest4.gcda 	new file:   cardtest4.gcno 	new file:   dominion.c.gcov 	modified:   dominion.gcda 	modified:   dominion.gcno 	deleted:    playdom.gcda 	deleted:    playdom.gcno 	modified:   rngs.gcda 	deleted:    rngs.gcno 	modified:   unittest1 	new file:   unittest1.gcda 	new file:   unittest1.gcno 	modified:   unittest2 	new file:   unittest2.gcda 	new file:   unittest2.gcno 	modified:   unittest3 	new file:   unittest3.gcda 	new file:   unittest3.gcno 	modified:   unittest4 	new file:   unittest4.gcda 	new file:   unittest4.gcno 	new file:   unittestresults.out

--- projects/solbracm/dominion/cardtest2.c
+++ projects/solbracm/dominion/cardtest2.c
@@ -15,9 +15,35 @@ int main(){
         initializeGame(MAX_PLAYERS, k, 5, &state);
         int coin_bonus=0;
 
-        // Check to see if the card draws 3
+        // Check to see if the at least two coins get added 
         
+	int i =  state.coins;	
+	
         cardEffect(7, -1, -1, -1, &state, 0, &coin_bonus);
- 
+	updateCoins(0, &state, 0);
+	int j = state.coins;
+
+	if(i+1 < j)
+		printf("Adventurer card PASSED treasure test\n");
+	else
+		printf("Adventurer card FAILED treasure test\n");
+
+
+	// ensure that the deck shuffles
+	
+
+	state.deckCount[0] = 0;
+
+
+        cardEffect(7, -1, -1, -1, &state, 0, &coin_bonus);
+
+
+	
+	if(state.deckCount[0] > 0)
+                printf("Adventurer card PASSED the shuffle test\n");
+        else
+                printf("Adventurer card FAILED the shuffle test\n");
+	
+
 	return(0);       
 }
